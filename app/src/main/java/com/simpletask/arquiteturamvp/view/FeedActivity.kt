08272815ecd5d0a85bbfabcff0f0f7884d077796
francovinicius <duartevini.dev@gmail.com>
2023-10-01 19:55:42 -0300

    reutilizando o presenter

--- app/src/main/java/com/simpletask/arquiteturamvp/view/FeedActivity.kt
+++ app/src/main/java/com/simpletask/arquiteturamvp/view/FeedActivity.kt
@@ -1,4 +1,43 @@
 package com.simpletask.arquiteturamvp.view
 
-class FeedActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.simpletask.arquiteturamvp.databinding.ActivityFeedBinding
+import com.simpletask.arquiteturamvp.model.Usuario
+import com.simpletask.arquiteturamvp.presenter.IUsuario
+import com.simpletask.arquiteturamvp.presenter.UsuarioPresenter
+
+class FeedActivity : AppCompatActivity(), IUsuario {
+
+    private  val binding by lazy {
+        ActivityFeedBinding.inflate(layoutInflater)
+    }
+    private lateinit var usuarioPresenter: UsuarioPresenter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        usuarioPresenter = UsuarioPresenter(this)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        usuarioPresenter.recuperarUsuarios()
+    }
+
+    override fun exibirUsuarios( lista: List<Usuario> ){
+
+        //Exibir os dados
+
+        var txtUsuarios = ""
+        lista.forEach{ usuario ->
+            txtUsuarios += "${usuario.nome} - ${usuario.idade} \n"
+        }
+
+        Log.i("exibir presenter", "$txtUsuarios")
+
+    }
+
 }
